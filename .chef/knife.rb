bfca63fef21319c8fc5e4083dfcbea86c78d9ef2
Cameron Johnston <cameron@rootdown.net>
2014-11-26 11:33:29 -0700

    add Gemfile, knife.rb and gitignore
    (cherry picked from commit c49f4243e84ab9c929526a47cbaa2039bece449c)

---
+++ .chef/knife.rb
@@ -0,0 +1,6 @@
+knife[:cloudformation][:processing] = true
+knife[:cloudformation][:credentials] = {
+  :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+  :aws_region => ENV['AWS_DEFAULT_REGION']
+}
---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem 'sparkle_formation'
+gem 'knife-cloudformation'
