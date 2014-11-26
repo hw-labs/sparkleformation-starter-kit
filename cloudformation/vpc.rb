8ef4e566414b618e4e8fbd4edd4a07ac64d01752
Cameron Johnston <cameron@rootdown.net>
2014-11-26 15:55:45 -0700

    add vpc template

---
+++ cloudformation/vpc.rb
@@ -0,0 +1,14 @@
+SparkleFormation.new(:coolapp_vpc).load(:vpc).overrides do
+
+  parameters(:public_subnet_availability_zone) do
+    type 'String'
+    default 'us-west-2a'
+  end
+
+  dynamic!(:subnet, 'public',
+      :vpc_id => ref!(:vpc),
+      :route_table => ref!(:public_route_table),
+      :availability_zone => ref!(:public_subnet_availability_zone)
+  )
+
+end
